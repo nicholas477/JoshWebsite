5e929f53f5f0e2ecfd32eeda9c78e38b36a00af0
EPICGameGuy <admin@cactus.vg>
2024-09-28 20:04:11 -0400

    Added file logging

--- log.py
+++ log.py
@@ -1,9 +1,35 @@
 from termcolor import colored
 import sys
 import datetime
+from contextlib import redirect_stdout
+
+class Tee:
+    def __init__(self, *files):
+        self.files = files
+
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush()
+
+    def flush(self):
+        for f in self.files:
+            f.flush()
+
+def open_log_file():
+    now = datetime.datetime.now()
+    return open(now.strftime("log_%Y-%m-%d__%I-%M-%S-%p.log"), 'w')
+
+log_file = open_log_file()
+
+# Tee stdout to console + log file
+original_stdout = sys.stdout
+sys.stdout = Tee(sys.stdout, log_file)
 
 xprint = print
 def print(*args, **kwargs):
     now = datetime.datetime.now()
-    sys.stdout.write(colored(now.strftime("[%Y-%m-%d %I:%M:%S %p] "), "green"))
+    datestr = now.strftime("[%Y-%m-%d %I:%M:%S %p] ")
+
+    sys.stdout.write(colored(datestr, "green"))
     xprint(*args, **kwargs)
